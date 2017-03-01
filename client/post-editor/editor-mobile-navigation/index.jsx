9c9b0926df14b01be10ee002e97b74123783a531
Matías Ventura <mv@matiasventura.com>
2017-03-01 16:50:14 +0100

    Mobile: simplify DOM structure.

--- client/post-editor/editor-mobile-navigation/index.jsx
+++ client/post-editor/editor-mobile-navigation/index.jsx
@@ -11,7 +11,6 @@ import Gridicon from 'gridicons';
  */
 import { setLayoutFocus } from 'state/ui/layout-focus/actions';
 import { getCurrentLayoutFocus } from 'state/ui/layout-focus/selectors';
-import EditorPublishButton from 'post-editor/editor-publish-button';
 import Button from 'components/button';
 
 const EditorMobileNavigation = React.createClass( {
@@ -48,11 +47,6 @@ const EditorMobileNavigation = React.createClass( {
 		return (
 			<div className="editor-mobile-navigation">
 				<div className="editor-mobile-navigation__actions">
-					<Button borderless onClick={ this.props.onClose }>
-						<Gridicon
-							icon="chevron-left"
-							className="editor-mobile-navigation__icon" />
-					</Button>
 					<div className="editor-mobile-navigation__tabs">
 						<Button borderless onClick={ this.closeSidebar }>
 							<Gridicon
@@ -70,16 +64,6 @@ const EditorMobileNavigation = React.createClass( {
 						</Button>
 					</div>
 				</div>
-				<EditorPublishButton
-					site={ this.props.site }
-					post={ this.props.post }
-					savedPost={ this.props.savedPost }
-					onSave={ this.props.onSave }
-					onPublish={ this.props.onPublish }
-					isPublishing={ this.props.isPublishing }
-					isSaveBlocked={ this.props.isSaveBlocked }
-					hasContent={ this.props.hasContent }
-				/>
 			</div>
 		);
 	}
